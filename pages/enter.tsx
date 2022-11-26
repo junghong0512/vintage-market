8fe33a0f6d5dcde651e40422c6e17c629d46c967
Jung Hong <junghong@ninewatt.com>
2022-11-27 02:11:43 +0900

    [UPDATE] #5.6 item upload page

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -41,13 +41,14 @@ export default function Enter() {
           </div>
         </div>
         <form className='flex flex-col mt-8'>
-          <label className='text-sm font-medium text-gray-700'>
+          <label htmlFor='input' className='text-sm font-medium text-gray-700'>
             {method === 'email' ? 'Email address' : null}
             {method === 'phone' ? 'Phone number' : null}
           </label>
           <div className='mt-1'>
             {method === 'email' ? (
               <input
+                id='input'
                 type='email'
                 className='appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500'
                 required
@@ -59,6 +60,7 @@ export default function Enter() {
                   +82
                 </span>
                 <input
+                  id='input'
                   type='number'
                   className='appearance-none w-full px-3 py-2 border border-gray-300 rounded-r-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500'
                   required
@@ -66,7 +68,7 @@ export default function Enter() {
               </div>
             ) : null}
           </div>
-          <button className='mt-5 bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none '>
+          <button className='mt-5 bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none'>
             {method === 'email' ? 'Get login link' : null}
             {method === 'phone' ? 'Get one-time password' : null}
           </button>
