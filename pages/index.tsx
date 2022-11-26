78dfd2ead282b821811669935b8f040d02354f6f
Jung Hong <junghong@ninewatt.com>
2022-11-26 19:07:37 +0900

    #5.0 ~ #5.3, Auth Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,11 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-sky-400'>
-      <div className='text-sky-900'>Test</div>
+    <div className='dark:md:hover:bg-teal-400'>
+      <h2 className='text-[97851px] text-[#000]'>Hello</h2>
     </div>
   );
+  return null;
 };
 
 export default Home;
