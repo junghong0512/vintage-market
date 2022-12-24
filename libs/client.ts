5733cb87df572c78fc3fb6f56b09650706e60e4a
Jung Hong <junghong91@naver.com>
2022-12-24 16:13:32 +0900

    [UPDATE] #6.7 API routes

--- libs/client.ts
+++ libs/client.ts
@@ -1,10 +1,3 @@
 import { PrismaClient } from '@prisma/client';
 
-const client = new PrismaClient();
-
-client.user.create({
-  data: {
-    email: 'junghong@ninewatt.com',
-    name: 'junghong',
-  },
-});
+export default new PrismaClient();
