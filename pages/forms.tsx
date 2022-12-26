3248a8bb8ae9a1eae6e28e674c524612a87a2aef
Jung Hong <junghong91@naver.com>
2022-12-26 17:57:39 +0900

    [UPDATE] how to use react-hook-form setErrors, reset...

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -4,18 +4,28 @@ interface LoginForm {
   username: string;
   password: string;
   email: string;
+  errors?: string;
 }
 
 export default function Forms() {
-  const { register, watch, handleSubmit } = useForm<LoginForm>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    reset,
+  } = useForm<LoginForm>({ mode: 'onChange' });
 
   const onValid = (data: LoginForm) => {
     console.log('Data is Valid');
+    // setError('errors', { message: 'Axios Error Occured' });
+    reset(); // reset all the inputs
   };
 
   const onInvalid = (errors: FieldErrors) => {
     console.log(errors);
   };
+  console.log(errors);
 
   return (
     <form onSubmit={handleSubmit(onValid, onInvalid)}>
@@ -31,16 +41,26 @@ export default function Forms() {
         placeholder='Username'
         required
       />
+      {errors.username?.message}
       <input
-        {...register('email', { required: true })}
+        {...register('email', {
+          required: true,
+          validate: {
+            notGmail: (value) =>
+              !value.includes('@gmail.com') || 'Gamil is not allowed',
+          },
+        })}
         type='email'
         placeholder='Email'
+        className={`${Boolean(errors.email) ? 'border-red-500' : ''}`}
       />
+      {errors.email?.message}
       <input
         {...register('password', { required: true })}
         type='password'
         placeholder='Password'
       />
+      {errors.errors?.message}
     </form>
   );
 }
