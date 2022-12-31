e737e37aa79916eccd2e1ff3bd31e0a67f377975
Jung Hong <junghong91@naver.com>
2022-12-31 19:45:05 +0900

    [UPDATE] Token Logic, connectOrCreate

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -5,18 +5,35 @@ import client from '@libs/server/client';
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
   const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  // const user = await client.user.upsert({
+  //   where: {
+  //     ...payload,
+  //   },
+  //   create: {
+  //     name: 'Anonymous',
+  //     ...payload,
+  //   },
+  //   update: {},
+  // });
+
+  const token = await client.token.create({
+    data: {
+      payload: '1234',
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: 'Anonymous',
+            ...payload,
+          },
+        },
+      },
     },
-    create: {
-      name: 'Anonymous',
-      ...payload,
-    },
-    update: {},
   });
 
-  console.log(user);
+  console.log(token);
 
   return res.status(200).end();
 }
