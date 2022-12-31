d0632c1c9b7d190d04662490c3672ed8eef54e4d
Jung Hong <junghong91@naver.com>
2022-12-31 20:34:12 +0900

    [UPDATE] TWILIO Setup

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,41 +1,34 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import withHandler from '@libs/server/withHandler';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
 import client from '@libs/server/client';
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>,
+) {
   const { phone, email } = req.body;
-  const payload = phone ? { phone: +phone } : { email };
-  // const user = await client.user.upsert({
-  //   where: {
-  //     ...payload,
-  //   },
-  //   create: {
-  //     name: 'Anonymous',
-  //     ...payload,
-  //   },
-  //   update: {},
-  // });
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const payload = Math.floor(100000 + Math.random() * 900000) + '';
 
   const token = await client.token.create({
     data: {
-      payload: '1234',
+      payload,
       user: {
         connectOrCreate: {
           where: {
-            ...payload,
+            ...user,
           },
           create: {
             name: 'Anonymous',
-            ...payload,
+            ...user,
           },
         },
       },
     },
   });
 
-  console.log(token);
-
-  return res.status(200).end();
+  return res.json({ ok: true });
 }
 
 export default withHandler('POST', handler);
