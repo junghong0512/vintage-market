052f44666cb5711b24dccc082ec294cc293908d5
Jung Hong <junghong91@naver.com>
2022-12-31 17:44:57 +0900

    [UPDATE] Account Logic of the enter page

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,23 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import withHandler from '@libs/server/withHandler';
+import client from '@libs/server/client';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: 'Anonymous',
+      ...payload,
+    },
+    update: {},
+  });
+
+  console.log(user);
+
   return res.status(200).end();
 }
 
