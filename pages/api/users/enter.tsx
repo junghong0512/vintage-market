3b3dfbd98f322d2f2195a3db1ac54cf0c0117e60
Jung Hong <junghong91@naver.com>
2022-12-31 21:22:24 +0900

    [UPDATE] #9.4 Sending SMS Using Twilio

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -2,6 +2,10 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import withHandler, { ResponseType } from '@libs/server/withHandler';
 import client from '@libs/server/client';
 
+import twilio from 'twilio';
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>,
@@ -27,6 +31,14 @@ async function handler(
       },
     },
   });
+
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MESSAGING_SID,
+      to: process.env.MY_PHONE_NUMBER!,
+      body: `Your login token is ${payload}.`,
+    });
+  }
 
   return res.json({ ok: true });
 }
