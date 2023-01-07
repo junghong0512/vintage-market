5f70b52bed85bde6830766a3c55f6195683c2b82
Jung Hong <junghong91@naver.com>
2023-01-07 18:53:08 +0900

    [UPDATE] #9.8 Profile Handler

--- pages/api/users/me.tsx
+++ pages/api/users/me.tsx
@@ -15,25 +15,17 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>,
 ) {
-  const { token } = req.body;
-  const exists = await client.token.findUnique({
-    where: {
-      payload: token,
-    },
+  const profile = await client.user.findUnique({
+    where: { id: req.session.user?.id },
   });
 
-  // 입력받은 토큰과 일치하는 유저 미존재
-  if (!exists) return res.status(404).end();
-
-  req.session.user = {
-    id: exists.userId,
-  };
-  await req.session.save();
-
-  res.status(200).end();
+  res.json({
+    ok: true,
+    profile,
+  });
 }
 
-export default withIronSessionApiRoute(withHandler('POST', handler), {
+export default withIronSessionApiRoute(withHandler('GET', handler), {
   cookieName: 'carrotsession',
   password: '2baWQGzT27nTzBinMGZeC81YbEf9F91T',
 });
