37a7a67ea6f4670caaa4f0d35379f611a369a16f
Jung Hong <junghong91@naver.com>
2023-01-13 19:02:14 +0900

    [UPDATE] #10.4 useUser Refactor using swr

--- libs/client/useUser.tsx
+++ libs/client/useUser.tsx
@@ -18,10 +18,15 @@ import useSWR from 'swr';
   return user;
 } */
 
-const fetcher = (url: string) => fetch(url).then((response) => response.json());
-
 export default function useUser() {
-  const { data, error } = useSWR('/api/users/me', fetcher);
+  const { data, error } = useSWR('/api/users/me');
   const router = useRouter();
-  return data;
+
+  useEffect(() => {
+    if (data && !data.ok) {
+      router.replace('/enter');
+    }
+  }, [data, router]);
+
+  return { user: data?.profile, isLoading: !data && !error };
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Layout from '@components/layout';
 import Head from 'next/head';
 
 const Home: NextPage = () => {
-  const user = useUser();
+  const { user, isLoading } = useUser();
   console.log(user);
 
   return (
