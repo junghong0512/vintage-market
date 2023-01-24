751ee2900512bd42ecd44469328359490d332e8f
Jung Hong <junghong91@naver.com>
2023-01-24 15:28:24 +0900

    [UPDATE] Favorite Product Backend

---
+++ pages/api/products/[id]/fav.ts
@@ -0,0 +1,54 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
+import client from '@libs/server/client';
+import { withApiSession } from '@libs/server/withSession';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>,
+) {
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+  const alreadyExists = await client.fav.findFirst({
+    where: {
+      productId: Number(id),
+      userId: user?.id,
+    },
+  });
+
+  if (alreadyExists) {
+    // delete
+    await client.fav.delete({
+      where: {
+        id: alreadyExists.id,
+      },
+    });
+  } else {
+    // create
+    await client.fav.create({
+      data: {
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+        product: {
+          connect: {
+            id: Number(id),
+          },
+        },
+      },
+    });
+  }
+
+  res.json({ ok: true });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ['POST'],
+    handler,
+  }),
+);
